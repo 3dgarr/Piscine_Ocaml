7a9d696910cedbbcddf0cf41dca6c432159d1d43
3dgarr <edgarghazaryandoc@gmail.com>
2025-02-02 22:30:10 +0400

    ex05 DONE

---
+++ Module01/ex05/ft_print_rev.ml
@@ -0,0 +1,17 @@
+let ft_print_rev str = 
+  let rec print_symbol str index = 
+    if index <= 0 then 
+      print_char '\n'
+    else
+      begin
+        print_char (String.get str (index - 1));
+        print_symbol str (index - 1)
+      end
+  in
+    print_symbol str (String.length str);
+;;
+
+ft_print_rev "barev";
+ft_print_rev "Hello World!";
+ft_print_rev "";
+ft_print_rev "1234567890";
