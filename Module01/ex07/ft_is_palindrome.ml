8bddd64b2edfe623f8d389cf18489d7f61e3b9da
3dgarr <edgarghazaryandoc@gmail.com>
2025-02-06 23:32:27 +0400

    Ex07 DONE

---
+++ Module01/ex07/ft_is_palindrome.ml
@@ -0,0 +1,20 @@
+let ft_is_palindrome str = 
+  let rec check_palindrome str start_index end_index = 
+    if start_index >= end_index then 
+      true
+    else if (String.get str start_index) <> (String.get str end_index) then 
+      false
+    else
+      check_palindrome str (start_index + 1) (end_index - 1)
+  in 
+    check_palindrome str 0 (String.length str - 1);
+
+;;
+
+
+print_endline (string_of_bool (ft_is_palindrome "0123456789"));;   
+print_endline (string_of_bool (ft_is_palindrome "madam"));;   
+print_endline (string_of_bool (ft_is_palindrome "apapa"));;   
+print_endline (string_of_bool (ft_is_palindrome "radar"));;   
+print_endline (string_of_bool (ft_is_palindrome "car"));;   
+print_endline (string_of_bool (ft_is_palindrome ""));;   
