0171753264501e918f6ba4a71ced65e3beec6612
3dgarr <edgarghazaryandoc@gmail.com>
2025-02-16 23:03:11 +0400

    Finishing Combs

---
+++ Module01/ex04/ft_print_comb.ml
@@ -0,0 +1,40 @@
+let print hundred decimal unit =
+    print_int hundred;
+    print_int decimal;
+    print_int unit;
+    if hundred = 7 && decimal = 8 && unit = 9 then 
+		print_string "\n"
+    else 
+		print_string ", "
+
+
+let rec get_units hundred decimal unit =
+    if unit <= 9 then
+        begin
+            print hundred decimal unit;
+            get_units hundred decimal (unit + 1)
+        end
+
+let rec get_decimals hundred decimal unit =
+    if decimal <= 8 then
+        begin
+            get_units hundred decimal unit;
+            get_decimals hundred (decimal + 1) (decimal + 2)
+        end
+
+let rec get_hundreds hundred decimal unit =
+    if hundred <= 7 then
+        begin
+            get_decimals hundred decimal unit;
+            get_hundreds (hundred + 1) (hundred + 2) (hundred + 3)
+        end
+
+let ft_print_comb () =
+    let hundred = 0 in
+	let decimal = hundred + 1 in
+	let unit = decimal + 1 in
+	get_hundreds hundred decimal unit
+
+;;
+
+let () = ft_print_comb()
---
+++ Module01/ex09/ft_print_comb2.ml
@@ -0,0 +1,40 @@
+let print_number decimal unit = 
+  if decimal < 10 then print_int 0;
+  print_int decimal;
+  print_char ' ';
+  if unit < 10 then print_int 0;
+  print_int unit;
+  if decimal == 98 && unit == 99 then
+    print_char '\n'
+  else
+    begin
+      print_char ',';
+      print_char ' '
+    end
+
+
+let rec get_unit decimal unit = 
+  if unit <= 99 then
+    begin 
+      print_number decimal unit ;
+      get_unit decimal (unit + 1)
+    end
+
+
+let rec get_decimal decimal unit  = 
+  if decimal <= 98 then
+    begin
+      get_unit  decimal unit ;
+      get_decimal (decimal + 1) (unit + 1)
+    end
+
+
+
+let ft_print_comb2 () = 
+  let decimal = 0 in
+  let unit = decimal + 1 in
+  get_decimal decimal unit
+
+;;
+
+let () = ft_print_comb2 ()
