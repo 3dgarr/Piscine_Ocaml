e9881406dd4f663e80a117ad93159a2fb6ef1995
3dgarr <edgarghazaryandoc@gmail.com>
2025-08-17 20:38:24 +0400

    ex06 done

---
+++ Module05/ex06/examples_of_file.ml
@@ -0,0 +1,30 @@
+let examples_of_file filename =
+  let ic = open_in filename in
+  let rec read_lines acc =
+    try
+      let line = input_line ic in
+      let parts = String.split_on_char ',' line in
+      let len = List.length parts in
+      if len = 0 then read_lines acc else
+      let floats =
+        Array.of_list (List.map float_of_string (List.rev (List.tl (List.rev parts))))
+      in
+      let cls = List.nth parts (len - 1) in
+      read_lines ((floats, cls) :: acc)
+    with End_of_file ->
+      close_in ic;
+      List.rev acc
+  in
+  read_lines []
+
+
+let () =
+  (* let examples = examples_of_file "/home/edgghaza/Desktop/42/Piscine_Ocaml/Module05/attachments.1.00/attachment/ionosphere.test.csv" in *)
+  (* let examples = examples_of_file "/home/edgghaza/Desktop/42/Piscine_Ocaml/Module05/attachments.1.00/attachment/ionosphere.train.csv" in *)
+  let examples = examples_of_file "/home/edgghaza/Desktop/42/Piscine_Ocaml/Module05/ex06/a.csv" in
+  List.iter
+    (fun (arr, cls) ->
+       Array.iter (fun f -> Printf.printf "%f " f) arr;
+       Printf.printf " -> %s\n" cls)
+    examples
+
