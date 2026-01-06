b447085930fb66ba46e121c0cbe272780987d94d
3dgarr <edgarghazaryandoc@gmail.com>
2026-01-06 15:01:23 +0400

    clean-up

--- Module09/ex04/main.ml
+++ Module09/ex04/main.ml
@@ -11,31 +11,31 @@ let equal_set s1 s2 =
   for_all s2 (fun x -> exists s1 (fun y -> x = y))
 
 let () =
-  (* return *)
   let s1 = return 1 in
   assert (exists s1 (fun x -> x = 1));
   assert (for_all s1 (fun x -> x = 1));
   print_int_set s1;
+  print_endline "Return test passed";
 
-  (* union *)
   let s2 = union (return 1 |> union (return 2)) (return 2 |> union (return 3)) in
   print_int_set s2;
   assert (equal_set s2 (union (return 1) (union (return 2) (return 3))));
+  print_endline "Union test passed";
 
-  (* inter *)
   let s3 = inter (union (return 1) (union (return 2) (return 3)))
                  (union (return 2) (union (return 3) (return 4))) in
   print_int_set s3;
   assert (equal_set s3 (union (return 2) (return 3)));
+  print_endline "Inter test passed";
 
-  (* diff *)
+
   let s4 = diff (union (return 1) (union (return 2) (return 3))) (return 2) in
   print_int_set s4;
   assert (exists s4 (fun x -> x = 1));
   assert (exists s4 (fun x -> x = 3));
   assert (not (exists s4 (fun x -> x = 2)));
+  print_endline "Diff test passed";
 
-  (* filter *)
   let s5 =
     filter
       (union (return 1) (union (return 2) (union (return 3) (return 4))))
@@ -46,7 +46,8 @@ let () =
   assert (exists s5 (fun x -> x = 4));
   assert (not (exists s5 (fun x -> x = 1)));
 
-  (* bind *)
+  print_endline "Filter test passed";
+
   let s6 =
     bind
       (union (return 1) (union (return 2) (return 3)))
@@ -55,13 +56,14 @@ let () =
   print_int_set s6;
   assert (exists s6 (fun x -> x = 10));
   assert (exists s6 (fun x -> x = 30));
+  print_endline "Bind test passed";
 
-  (* for_all *)
   assert (for_all (union (return 2) (union (return 4) (return 6))) (fun x -> x mod 2 = 0));
   assert (not (for_all (union (return 2) (union (return 3) (return 4))) (fun x -> x mod 2 = 0)));
+  print_endline "For_all test passed";
 
-  (* exists *)
   assert (exists (union (return 1) (union (return 3) (return 4))) (fun x -> x mod 2 = 0));
   assert (not (exists (union (return 1) (union (return 3) (return 5))) (fun x -> x mod 2 = 0)));
+  print_endline "Exists test passed";
 
   print_endline "All Set monad tests passed"
